6f50556f115bb1cfd190c3f998e2ece80e74207c
omar <omarvargas914@gmail.com>
2021-01-12 01:54:26 -0500

    commit tree b325d268ccc75d9f1998d37bb05b76f83059e497

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
 # Build your say_hello method here
-def say_hello(name)
+def say_hello(name = "Ruby Programmer")
     puts "Hello #{name}!"
 end
-def say_hello_noname(name = "Ruby programmer")
-    puts "Hello #{name}!"
-end
-say_hello_noname
+
+
+
+
